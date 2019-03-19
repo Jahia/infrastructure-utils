01feac547bcc05fd0efd986f9084e3e4740b9811
Damien <dsaulnier@jahia.com>
2019-03-19 11:44:45 +0000

    First commit: - servlet to retrieve the status of the readonly mode and enable/disable it - bundle listener to automatically undeploy old versions of modules on a continuous integration platform

---
+++ src/main/java/org/jahia/modules/infrastructure/osgi/ModuleVersionsPurgeActivator.java
@@ -0,0 +1,68 @@
+package org.jahia.modules.infrastructure.osgi;
+
+import org.jahia.services.SpringContextSingleton;
+import org.jahia.services.modulemanager.BundleInfo;
+import org.jahia.services.modulemanager.ModuleManager;
+import org.jahia.settings.SettingsBean;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.BundleListener;
+import org.osgi.framework.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+
+public class ModuleVersionsPurgeActivator implements BundleActivator {
+
+    private static final Logger logger = LoggerFactory.getLogger(ModuleVersionsPurgeActivator.class);
+
+    private BundleListener bundleListener;
+    private int nbFormerVersionToKeep = 1;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        if (!SettingsBean.getInstance().isProcessingServer()) return;
+        final String isActive = SettingsBean.getInstance().getPropertiesFile().getProperty("modules.versions.autoPurge", "false");
+        if (!"true".equalsIgnoreCase(isActive.trim())) return;
+
+        context.addBundleListener(bundleListener = new BundleListener() {
+
+            @Override
+            public void bundleChanged(BundleEvent event) {
+                if (event.getType() != BundleEvent.STARTED) {
+                    return;
+                }
+                final Bundle changedBundle = event.getBundle();
+                if (changedBundle == null) return;
+                final Object header = changedBundle.getHeaders().get("Jahia-CI-Purge");
+                if (!(header instanceof String) || !"true".equalsIgnoreCase((String) header)) return;
+                final String symbolicName = changedBundle.getSymbolicName();
+                final Version version = changedBundle.getVersion();
+                logger.info(String.format("Bundle %s-%s started, let's scan for lower versions installed", symbolicName, version.toString()));
+                for (Bundle b : context.getBundles()) {
+                    if (b.getSymbolicName().equals(symbolicName) && b.getVersion().compareTo(version) < 0) {
+                        logger.info(String.format("Detected a module to purge: %s-%s (%s)", b.getSymbolicName(), b.getVersion().toString(), b.getBundleId()));
+                        try {
+                            final ModuleManager moduleManager = (ModuleManager) SpringContextSingleton.getBean("ModuleManager");
+                            moduleManager.uninstall(BundleInfo.fromBundle(changedBundle).getKey(), null);
+                            b.uninstall();
+                        } catch (BundleException e) {
+                            logger.error(String.format("Impossible to uninstall the bundle %s-%s (%s)", b.getSymbolicName(), b.getVersion().toString(), b.getBundleId()), e);
+                        } catch (NoSuchBeanDefinitionException nsbde) {
+                            logger.error("Impossible to load the ModuleManager");
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        if (bundleListener != null)
+            context.removeBundleListener(bundleListener);
+    }
+}
---
+++ src/main/java/org/jahia/modules/infrastructure/servlet/FullReadonlyModeServlet.java
@@ -0,0 +1,131 @@
+package org.jahia.modules.infrastructure.servlet;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.gemini.blueprint.context.BundleContextAware;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.settings.readonlymode.ReadOnlyModeController;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FullReadonlyModeServlet extends HttpServlet implements BundleContextAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(FullReadonlyModeServlet.class);
+    BundleContext bundleContext;
+
+    @Override
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    public void onBind(ServiceReference serviceReference) {
+        final ServiceReference realServiceReference = bundleContext.getServiceReference(HttpService.class.getName());
+        final HttpService httpService = (HttpService) bundleContext.getService(realServiceReference);
+        try {
+            httpService.registerServlet("/infrastructure/readonlymode", this, null, null);
+            logger.info("Successfully registered custom servlet at /modules/infrastructure/readonlymode");
+        } catch (ServletException | NamespaceException e) {
+            logger.error("", e);
+        }
+
+    }
+
+    public void onUnbind(ServiceReference serviceReference) {
+
+        if (serviceReference == null) {
+            return;
+        }
+        final ServiceReference realServiceReference = bundleContext.getServiceReference(HttpService.class.getName());
+        if (realServiceReference == null) {
+            return;
+        }
+        final HttpService httpService = (HttpService) bundleContext.getService(realServiceReference);
+        if (httpService == null) {
+            return;
+        }
+        httpService.unregister("/infrastructure/readonlymode");
+        logger.info("Successfully unregistered custom servlet from /modules/infrastructure/readonlymode");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final JSONObject result = new JSONObject();
+        final PrintWriter writer = response.getWriter();
+        try {
+            if(!isUserAllowed()) {
+                result.put("error", "Unsufficient privileges");
+                disableClientSideCache(response);
+                writer.println(result.toString());
+                return;
+            }
+
+            String pathInfo = request.getPathInfo();
+            if (pathInfo == null) pathInfo = StringUtils.EMPTY;
+            final String action = pathInfo.toUpperCase();
+            switch (action) {
+                case "/ENABLE":
+                    try {
+                        ReadOnlyModeController.getInstance().switchReadOnlyMode(true);
+                        result.put("action", "Enabled the readonly mode");
+                    } catch (IllegalStateException ignored) {
+                        // already in readonly mode
+                    }
+                    break;
+                case "/DISABLE":
+                    try {
+                        ReadOnlyModeController.getInstance().switchReadOnlyMode(false);
+                        result.put("action", "Disabled the readonly mode");
+                    } catch (IllegalStateException ignored) {
+                        // already out of readonly mode
+                    }
+                    break;
+                case StringUtils.EMPTY:
+                    break;
+                default:
+                    logger.error("Unexpected action type: " + action);
+                    result.put("error", String.format("Unexpected action: %s", action));
+                    disableClientSideCache(response);
+                    writer.println(result.toString());
+                    return;
+            }
+            result.put("status", ReadOnlyModeController.getInstance().getReadOnlyStatus());
+        } catch (JSONException e) {
+            logger.error("", e);  //TODO: review me, I'm generated
+        }
+
+        disableClientSideCache(response);
+        writer.println(result.toString());
+    }
+
+    private void disableClientSideCache(HttpServletResponse response) {
+        response.setHeader("Expires", "Mon, 26 Jul 1990 05:00:00 GMT");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+    }
+
+    private boolean isUserAllowed() {
+
+        try {
+            final JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession();
+            return session.getUser().isRoot();
+        } catch (RepositoryException e) {
+            logger.error("", e);
+            return false;
+        }
+
+    }
+}
